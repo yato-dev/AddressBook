49abf84b621e727699886ef5a751d25979ff8d6a
Shikhar Barthwal <barthwalshikhar@gmail.com>
2023-12-26 01:38:39 +0530

    modified uc2

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 class AddressBook {
     private List<Contact> contacts;
@@ -11,6 +12,37 @@ class AddressBook {
     public void addContact(Contact contact) {
         contacts.add(contact);
         System.out.println("Contact added successfully!");
+    }
+
+    public void editContact(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getFirstName().equalsIgnoreCase(name) || contact.getLastName().equalsIgnoreCase(name)) {
+                // Match found, allow editing
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Enter new address:");
+                contact.setAddress(scanner.nextLine());
+                System.out.println("Enter new city:");
+                contact.setCity(scanner.nextLine());
+                System.out.println("Enter new state:");
+                contact.setState(scanner.nextLine());
+                System.out.println("Enter new zip:");
+                contact.setZip(scanner.nextLine());
+                System.out.println("Enter new phone number:");
+                contact.setPhoneNumber(scanner.nextLine());
+                System.out.println("Enter new email:");
+                contact.setEmail(scanner.nextLine());
+
+                System.out.println("Contact edited successfully!");
+                scanner.close();
+                return;
+            }
+        }
+
+        // If the loop completes, no matching contact found
+        System.out.println("No contact found with the name: " + name);
+
+       
     }
 
     public void displayContacts() {
